5b2a8f2dfc03cfef70fa1cabe6d503e8b8037fea
Beatriz <beatrizkloss@gmail.com>
2024-08-02 14:36:09 -0300

    controle de fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        int parametroUm = 0;
+        int parametroDois = 0;
+
+        // Tratamento de exceção para entrada inválida
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            parametroDois = terminal.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida. Por favor, insira números inteiros.");
+            return;
+        }
+
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é maior que parametroDois e lançar a exceção
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        // Realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
